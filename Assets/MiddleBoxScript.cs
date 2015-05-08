f551f7b3a637f717984e1ee1478eb62d05597a0d
Shazmil Nassar <sham21786@yahoo.com>
2015-05-08 15:12:54 +0100

    week 6

---
+++ Assets/MiddleBoxScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MiddleBoxScript : MonoBehaviour {
+
+    public Transform myTarget;
+
+	void Start () 
+    {
+        StartCoroutine(myThang());
+	}
+
+    IEnumerator myThang () 
+    {
+        for (int i = 0; i < 50; i++)
+        {
+            transform.Translate(transform.up * Time.deltaTime * 4);
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(1);
+
+        for (int i = 0; i < 49 ; i++)
+        {
+            transform.Translate(transform.right * Time.deltaTime * 4);
+            yield return null;
+        }
+
+        yield return new WaitForSeconds (1);
+
+        for (int i = 0; i < 43; i++)
+        {
+            transform.Translate(Vector3.down * Time.deltaTime * 4);
+            yield return null;
+        }
+        
+        yield return new WaitForSeconds (1);
+
+        myTarget.SendMessage("DoMahThang");
+    }
+}
---
+++ Assets/TopBox.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TopBox : MonoBehaviour {
+
+    void DoMahThang()
+    {
+        StartCoroutine(lidClose());
+    }
+
+    IEnumerator lidClose()
+    {
+        for (int i = 0; i < 46; i++)
+        {
+            transform.Translate(Vector3.down * Time.deltaTime * 4);
+            yield return null;
+        }
+    }
+}
